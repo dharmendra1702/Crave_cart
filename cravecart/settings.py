5c8c39f79e11493b069afc2d8720bec924bee703
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2025-12-31 08:07:11 +0530

    Fix STATIC_ROOT for Render

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Razepay Key ID and Secret Key
 RAZORPAY_KEY_ID = 'rzp_test_RxlrQpOeLpU4vA'
