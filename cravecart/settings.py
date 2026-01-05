8b3201d288c7573cfbab524248d93655f138e54f
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-01-05 19:59:02 +0530

    Fix: add whitenoise for production static files

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aq2(8)y!1rjs89d)r&ufd+sxh#ez&+7qtxux+*!4hc8-h6hfg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
