a58d9d8934b3281abec830964ccefc9825d2cfb4
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-01-08 11:19:39 +0530

    Fix recursive URLs and architecture

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -67,11 +67,14 @@ INSTALLED_APPS = [
 
 
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.environ.get("CLOUDINARY_API_KEY"),
-    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    secure=True,
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+    secure=True
 )
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 
 MIDDLEWARE = [
