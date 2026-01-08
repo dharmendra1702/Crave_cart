402ff8816f9c03ed97193cd4ac5e3aed3b6d61db
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-01-08 10:10:35 +0530

    Permanent fix: cart, session, static, duplicate views

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -180,11 +180,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 
