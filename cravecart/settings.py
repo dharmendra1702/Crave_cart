976e03feedc4583ad74ef85fb7e024db47e34300
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-01-08 11:07:14 +0530

    Fix recursive URLs and architecture

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -65,18 +65,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
-    "STATICFILES_MANIFEST_STRICT": False
-}
 
 cloudinary.config(
     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
-    secure=True
+    secure=True,
 )
 
 
