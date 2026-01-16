b63e2c08dc8acfb6c53f208490c598dc8f606e89
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-01-16 22:38:00 +0530

    profile update

--- cravecart/settings.py
+++ cravecart/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'anymail'
 ]
 
 
@@ -211,9 +212,8 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STATICFILES_DIRS = []  # IMPORTANT (empty for Render)
 
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "true").lower() == "true"
 EMAIL_TIMEOUT = int(os.environ.get("EMAIL_TIMEOUT", "10"))
